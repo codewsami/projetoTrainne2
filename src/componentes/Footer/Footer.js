5b19c1cef30b113a619c420220326c2dc08c67c7
codewsami <samira.messsias.lins@gmail.com>
2025-07-26 23:54:16 -0300

    header e footer

--- src/componentes/Footer/Footer.js
+++ src/componentes/Footer/Footer.js
@@ -8,38 +8,44 @@ const Footer = () => {
         <div className="footer-logo-section">
           <div className="footer-logo">
             <img src="/logo2.svg" alt="Logo" />
-            <div className="Text-logo">Simon Filmes</div>
+            <div className="text-logo">Simon Filmes</div>
           </div>
 
           <p className="about-us">ABOUT US</p>
         </div>
 
-        <div className="footer-text">
-          <div className="footer-column">
-            <h4>Núcleo de criação</h4>
-            <p className="texto1">
-              Ayla Costa <br></br>
-              Ayla Santos Giovanna Antunes <br></br>
-              Giovanna Antunes <br></br>
-            </p>
+        <div className="footer-column-group">
+          <div className="title-text">
+            <h3>Powered By</h3>
           </div>
+          <div className="footer-columns-container">
+            <div className="footer-text"></div>
+            <div className="footer-column">
+              <h4>Núcleo de criação</h4>
+              <p className="texto1">
+                Ayla Costa <br></br>
+                Ayla Santos <br></br>
+                Giovanna Antunes <br></br>
+              </p>
+            </div>
 
-          <div className="footer-column">
-            <h4>Núcleo de Front-end</h4>
-            <p className="texto1">
-              Danilo Nishimoto <br></br>
-              Nicolas Santos <br></br>
-              Samira Lins <br></br>
-            </p>
-          </div>
+            <div className="footer-column">
+              <h4>Núcleo de Front-end</h4>
+              <p className="texto1">
+                Danilo Nishimoto <br></br>
+                Nicolas Santos <br></br>
+                Samira Lins <br></br>
+              </p>
+            </div>
 
-          <div className="footer-column">
-            <h4>Núcleo de Back-end</h4>
-            <p className="texto1">
-              Giovanne Giroldo<br></br>
-              Nicolas Fagundes <br></br>
-              Vinícios Conceição <br></br>
-            </p>
+            <div className="footer-column">
+              <h4>Núcleo de Back-end</h4>
+              <p className="texto1">
+                Giovanne Giroldo<br></br>
+                Nicolas Fagundes <br></br>
+                Vinícios Conceição <br></br>
+              </p>
+            </div>
           </div>
         </div>
       </div>
